debb88efbddeccd9debda1cf49f785c838757d4d
Ruthvik <ruthvik@Ruthviks-MacBook-Pro.local>
2018-03-18 13:23:54 +0530

    added sprite and movement

--- state0.js
+++ state0.js
@@ -1,7 +1,10 @@
 var demo = {};
 demo.state0=function(){};
+var hero,cursors,speed=10;
+var centerX=1500/2,centerY=1000/2;
 demo.state0.prototype = {
     preload:function(){
+        game.load.image('hero','assets/hero.png');
         
     },
     
@@ -9,10 +12,27 @@ demo.state0.prototype = {
         console.log('state0');
         game.stage.backgroundColor = 'FF0000';
         addChangeStateEventListners();
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        
+        //hero
+         hero = game.add.sprite(centerX,centerY,'hero');
+        hero.anchor.setTo(0.5,0.5);
+        cursors = game.input.keyboard.createCursorKeys();
     },
     
     update:function(){
+        if(cursors.up.isDown)
+            hero.y -= speed;
         
+        else if(cursors.down.isDown)
+            hero.y += speed;
+        
+         if(cursors.left.isDown)
+            hero.x -= speed;
+        
+        
+        else if(cursors.right.isDown)
+            hero.x += speed;
     }
     
 };
